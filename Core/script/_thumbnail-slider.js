7304c9019b735199f45150d4cb920e2a663b1d4b
Muhammad Ali <muhammad.ali@salt.co.id>
2017-07-20 12:20:04 +0700

    [UPDATE] adding nav-category on blog

--- Core/script/_thumbnail-slider.js
+++ Core/script/_thumbnail-slider.js
@@ -148,6 +148,42 @@
 		]
 	});
 
+	$('.nav-category .category-slide').slick({
+		dots: true,
+		arrows: false,
+		autoplay: false,
+		speed: 300,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+		infinite: false,
+		speed: 500,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 5,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					adaptiveHeight: true,
+				}
+			}
+		]
+	});
+
 	// $('.gallery .gallery-slide').slick({
 	// 	dots: false,
 	// 	arrows: false,
